a933dbab02a2b4d923ea31ccc6a3a5ad9b2755d2
Matija Nalis <mnalis-git@voyager.hr>
2026-01-16 00:56:00 +0100

    initial version

---
+++ discourse_csv_to_mbox.pl
@@ -0,0 +1,96 @@
+#!/usr/bin/perl -T
+# by Matija Nalis <mnalis-perl@voyager.hr> under AGPLv3+, started 2026015
+#
+# converts Discourse "user_archive.csv" from user_archive*.zip data export ZIP file to MBOX format to be read in MUAs
+#
+
+use strict;
+use warnings;
+
+use Date::Parse;
+use POSIX qw(strftime);
+use Text::CSV qw ( csv );
+use open ':encoding(UTF-8)';
+use autodie qw(:all);
+use feature 'say';
+
+my $csv_file = 'user_archive.csv';
+
+my $user = 'discourse';
+my $domain = 'discourse.invalid';
+my $replace_domain = 1;
+
+#
+# no user serviceable parts below
+#
+
+my $row;
+
+# record optional header, if present
+sub add_opt_header($) {
+    my $h = shift;
+    my $v = $row->{$h};
+    
+    say "X-Discourse-$h: $v" if defined $v;
+}
+
+#
+# MAIN
+#
+
+
+# instead of "perl -CSD"
+binmode(STDOUT, ":encoding(UTF-8)");
+binmode(STDERR, ":encoding(UTF-8)");
+
+
+my $csv = Text::CSV->new({
+    binary      => 1,
+    strict      => 1,
+    auto_diag   => 1,
+});
+
+open my $fh, '<', $csv_file or die "Can't open $csv_file: $!";
+
+# read and set up CSV headers
+$csv->header ($fh);
+
+# parse CSV body
+while ($row = $csv->getline_hr($fh)) {
+
+    my $subject = $row->{'topic_title'} // '(no subject)';
+    my $body    = $row->{'post_raw'}    // '(no body)';  #FIXME: also 'post_cooked' for text/html in multipart-alternative?
+    my $created = $row->{'created_at'}  // '';
+    my $url     = $row->{'url'};
+    
+    if ($replace_domain && defined $url) {
+        if ($url =~ m{^https?://([^/]+)/}) { $domain = $1 }
+    }
+
+    my $epoch = str2time($created) || time;
+    my $date  = strftime("%a %b %d %H:%M:%S %Y", gmtime($epoch));
+#    my $msgid = join('.', ($epoch, time, $$, rand(1000), $url)) . '@' . $domain;   # use undermenistic random ID
+    my $msgid = join('.', ($epoch, $url)) . '@' . $domain; # semi-permanent ID, for detecting previous messages
+
+    
+    $body =~ s/^From /">From "/mg;  # escape any "From " lines if present, to avoid breaking MBOX format
+
+    say "From $user\@$domain $date";
+    say "From: $user\@$domain";
+    say "Subject: $subject";
+    say "Date: $date +0000";
+    say "Message-ID: <$msgid>";
+
+    add_opt_header('url');
+    add_opt_header('categories');
+    add_opt_header('is_pm');
+    add_opt_header('like_count');
+    add_opt_header('reply_count');
+
+    say 'Content-Type: text/plain; charset=UTF-8';
+    say '';
+    say $body;
+    say '';
+}
+
+close $fh;
